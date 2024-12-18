d2f7baad07cfb7019097bc6a2c7a461d9e96a424
Hanane Oubaha <Hananeouba03@gmail.com>
2024-12-18 12:09:03 +0100

    j'ai cree mon data base chef cuisine avec les tables  users roles menu plats  et reservations anisi mon uml use case de ce projet aprés une analuse bien détaillé des besoins de ce peojet

---
+++ script.sql
@@ -0,0 +1,53 @@
+
+
+CREATE DATABASE chef_cuisine;
+USE chef_cuisine;
+
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(50) NOT NULL UNIQUE
+);
+-- Table Users
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
+
+-- Table Menu
+CREATE TABLE menu (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table Plats
+CREATE TABLE plats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    menu_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    ingredients TEXT NOT NULL,
+    image VARCHAR(200) NOT NULL,
+    FOREIGN KEY (menu_id) REFERENCES menu(id) ON DELETE CASCADE
+);
+
+-- Table Reservations
+CREATE TABLE reservations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    menu_id INT NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    number_of_people INT NOT NULL,
+    status ENUM('en attente', 'confirme', 'annule') DEFAULT 'en attente',
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (menu_id) REFERENCES menu(id) ON DELETE CASCADE
+);
+
+
